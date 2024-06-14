ff9536ebceaad51981946f0a543ea3330e5c65e5
Zaracoszuckzarek <165440650+Zaracoszuckzarek@users.noreply.github.com>
2024-06-13 19:10:11 -0500

    update

--- PROJECTS/src/pages/Login.jsx
+++ PROJECTS/src/pages/Login.jsx
@@ -1,12 +1,56 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-
-export async function loader(){
-    return null
+export async function loader() {
+  return null;
 }
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  function handleFormChange(e){
+    const {name, value} =e.target
+    setLoginForm(prevLoginForm =>({
+        ...prevLoginForm,
+        [name]: value
+    }))
+  }
+
+  return (
+    <div className="loginForm">
+      <h1>Login form</h1>
+      <form>
+        <div>
+          <input
+            type="email"
+            name="email"
+            value={loginForm.email}
+            onChange={handleFormChange}
+            placeholder="email address"
+          />
+        </div>
+
+        <div>
+          <input
+            type="password"
+            name="password"
+            value={loginForm.password}
+            onChange={handleFormChange}
+            placeholder="password"
+          />
+        </div>
+        <span>
+        <Link to="forgotpassword">forgot password</Link>
+        <Link to="newaccount">new account</Link>
+        </span>
+    
+
+        <button>submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
